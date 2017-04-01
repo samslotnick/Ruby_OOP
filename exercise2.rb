197a8390e77ef41321b5d01a35f8d48c8c59aaa0
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-04-01 13:55:08 -0400

    ex2 almost done

--- exercise2.rb
+++ exercise2.rb
@@ -5,15 +5,24 @@ class Cats
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
+
   def eats_at(name)
-    if meal_time >= 12 then puts "#{@name} eats at #{@meal_time} PM"
+    if meal_time >= 12 then
+       puts "#{@name} eats at #{@meal_time -= 12} PM"
     else puts "#{@name} eats at #{@meal_time} AM"
     end
   end
+ def meow(name)
+   if @name == "Felix" then puts "I'm a Felix, polymath and optimist"
+   else puts "I'm thylvethter, I have a thpeech impediment."
+   end
+ end
+
 
 end
 
-felix = Cats.new("Felix", "Cheese", 6)
-sylvester = Cats.new("Sylvester","Birds", 18)
+#felix = Cats.new("Felix", "Cheese", 6)
+#sylvester = Cats.new("Sylvester","Birds", 19)
 
-puts felix.eats_at (@meal_time)
+#puts felix.eats_at (@meal_time)
+#puts sylvester.eats_at (@meal_time)
